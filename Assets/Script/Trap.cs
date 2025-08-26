096d661a3f6cd38d5ed0ae688a0599675c37b2fc
FilippFlip <f.mishenev@gmail.com>
2025-08-26 11:17:18 +0300

    lesson

--- Assets/Script/Trap.cs
+++ Assets/Script/Trap.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
+    public float explosionRadius;
+    private Collider[] skeletons;
     public ParticleSystem explosionEffect;
     private bool isTriggered;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,10 +21,20 @@ public class Trap : MonoBehaviour
     {
         if (other.tag == "skelet"&&isTriggered==false)
         {
+            skeletons = Physics.OverlapSphere(transform.position, explosionRadius);
+            foreach (Collider c in skeletons)
+            {
+                if (c.TryGetComponent(out EnemyController enemy))
+                {
+                    enemy.Death();
+                }
+
+            }
             isTriggered = true;
             explosionEffect.Play();
             await Awaitable.WaitForSecondsAsync(10);
             Destroy(gameObject);
+
         }
     }
 }
